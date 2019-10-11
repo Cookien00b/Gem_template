58d8535b593b6d5db7c68f2bee75a65d91d56fcb
merlijnbrinks <merlijnbrinks@gmail.com>
2019-10-11 21:33:05 +0200

    Template for gem making

---
+++ Template_Maker.js
@@ -0,0 +1,17 @@
+var Gem = prompt("Gem name");
+document.getElementById("Gem").innerHTML = Gem;
+
+var Weapon = prompt("Gems weapon");
+document.getElementById("Weapon").innerHTML = Weapon;
+
+var Appearance = prompt("Appearance");
+document.getElementById("Appearance").innerHTML = Appearance;
+
+var Ability = prompt("Ability");
+document.getElementById("Ability").innerHTML = Ability;
+
+var Skills = prompt("Skills");
+document.getElementById("Skills").innerHTML = Skills;
+
+var Other = prompt("Other");
+document.getElementById("Other").innerHTML = Other;
